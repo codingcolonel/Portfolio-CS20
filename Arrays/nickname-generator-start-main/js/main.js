c3ac48107ce0aeca4c9c9ae2f68c33ae1092f511
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-02-22 14:48:01 -0700

    nickname gen progress

--- Arrays/nickname-generator-start-main/js/main.js
+++ Arrays/nickname-generator-start-main/js/main.js
@@ -25,11 +25,25 @@ function btnClicked() {
   // Get Menu Selection
   let selection = document.getElementById('menu-select').value;
 
+  firstName = document.getElementById('firstNameIn').value;
+  lastName = document.getElementById('lastNameIn').value;
+
+  // Reset output elements
+  outputEl.innerHTML = '';
+
   // Process Menu Selection
   if (selection === 'allNick') {
-    allNicknames();
+    if (firstName !== '' && lastName !== '') {
+      allNicknames();
+    } else {
+      outputEl.innerHTML += `<p class='error'>Please enter a valid nickname</p>`;
+    }
   } else if (selection === 'randomNick') {
-    randomNickname();
+    if (firstName !== '' && lastName !== '') {
+      randomNickname();
+    } else {
+      outputEl.innerHTML += `<p class='error'>Please enter a valid nickname</p>`;
+    }
   } else if (selection === 'addNick') {
     addNickname();
   } else if (selection === 'removeNick') {
@@ -42,20 +56,46 @@ function btnClicked() {
 function allNicknames() {
   // Get all nicknames from array and output them
   for (let i = 0; i < nicknames.length; i++) {
-    // outputEl += `${}`
+    outputEl.innerHTML += `<p>${firstName} "${nicknames[i]}" ${lastName}</p>`;
   }
 }
 
 function randomNickname() {
   // Get random nickname from array and output it
+  outputEl.innerHTML += `<p>${firstName} "${
+    nicknames[randomInt(0, nicknames.length)]
+  }" ${lastName}</p>`;
 }
 
 function addNickname() {
-  // Ask user for new nickname and add it to the array
+  // Ask user for new nickname and add it to the end of the array
+  outputEl.innerHTML += `<p>New Nickname: <input type="text" id="newNicknameIn" class="input" /><button id="btn2">GO</button></p>`;
+
+  document.getElementById('btn2').addEventListener('click', addNicknameToArray);
+}
+
+function addNicknameToArray() {
+  let newNick = document.getElementById('newNicknameIn').value;
+
+  if (newNick !== '') {
+    nicknames.push(newNick);
+    outputEl.innerHTML = '';
+    outputEl.innerHTML += 'Nickname Submitted';
+    setTimeout(() => {
+      outputEl.innerHTML = '';
+    }, 1000);
+  } else {
+    outputEl.innerHTML += `<p class='error'>Please enter a valid nickname</p>`;
+  }
 }
 
 function removeNickname() {
   // Remove the newest nickname in the array
+  nicknames.pop();
+  outputEl.innerHTML += '<p>Removed most recent nickname</p>';
+  setTimeout(() => {
+    outputEl.innerHTML = '';
+  }, 1000);
 }
 
 function removeNicknameByIndex() {
