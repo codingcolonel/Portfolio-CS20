0bed5e29de1f921cb43aa4879de771d0a5d3b097
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-03-23 21:47:26 -0600

    Did some C# stuff

---
+++ C#/Hello World/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Hello World
+// Console.WriteLine("Hello, World!");
+
+
+
+// // Hello User
+// #nullable disable
+
+// Console.Write("Hello, What is your name? ");
+// string userName = Console.ReadLine();
+
+// Console.Write("Hello, What time of day is it? ");
+// string timeOfDay = Console.ReadLine();
+
+// Console.WriteLine($"\nHello {userName}! Have a great {timeOfDay}.");
+
+
+
+
+// Average Calculator
