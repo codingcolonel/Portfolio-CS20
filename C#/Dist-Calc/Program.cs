24ea308ddd3e8ab1be950f760329a2b46c406742
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-03-24 14:47:56 -0600

    done dist calc in c#

---
+++ C#/Dist-Calc/Program.cs
@@ -0,0 +1,26 @@
+﻿// Distance Calculator in C#
+
+
+// Welcome
+#nullable disable
+Console.Clear();
+Console.WriteLine("WELCOME TO THE DISTANCE CALCULATOR!");
+
+// Input
+Console.WriteLine("\nEnter x1 value: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter y1 value: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter x2 value: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter y2 value: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+// Process
+double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+// Output
+Console.WriteLine($"\nDistance between the two points is {dist}");
--- C#/Hello World/Program.cs
+++ C#/Hello World/Program.cs
@@ -4,7 +4,7 @@
 
 
 // // Hello User
-// #nullable disable
+#nullable disable
 
 // Console.Write("Hello, What is your name? ");
 // string userName = Console.ReadLine();
@@ -17,4 +17,90 @@
 
 
 
-// Average Calculator
+// // Average Calculator
+
+// // WELCOME
+// Console.Clear();
+// Console.WriteLine("Welcome to the Average Calculator.");
+
+// // INPUT
+// Console.Write("Write first number: ");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Write second number: ");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+
+// // PROCESS
+// double average = (num1 + num2) / 3;
+
+// // OUTPUT
+// Console.WriteLine($"The average is: {average}");
+
+
+
+// // KF Panda Characters
+
+// // WELCOME
+// Console.Clear();
+// Console.WriteLine("Welcome to the Average Calculator.");
+
+// // MAIN LOOP
+// while (true)
+// {
+//     // GET SERACH INPUT FROM USER
+//     Console.Write("\nWrite character name or quit: ");
+//     string name = Console.ReadLine().ToLower();
+
+//     // PROCESS INPUT
+//     if (name == "po" || name == "dragon warrior" || name == "kung fu panda")
+//     {
+//         Console.WriteLine("Skadoosh!");
+//     }
+//     else if (name == "mr. ping")
+//     {
+//         Console.WriteLine("There is no secret ingredient");
+//     }
+//     else if (name == "master shifu" || name == "shifu")
+//     {
+//         Console.WriteLine("If you only do what you can do, you will never be more than who you are.");
+//     }
+//     else if (name == "quit")
+//     {
+//         break;
+//     }
+//     else
+//     {
+//         Console.WriteLine("Character not found");
+//     }
+// }
+
+// Console.WriteLine("Goodbye");
+
+
+
+// Coin Flip SIm
+Console.Clear();
+
+// Create random object
+Random rnd = new Random();
+
+// Loop ? times and get a random number
+int headCount = 0;
+int tailCount = 0;
+for (int n = 1; n <= 1000; n++)
+{
+    int randNum = rnd.Next(1, 3);
+    if (randNum == 1)
+    {
+        Console.WriteLine("Heads");
+        headCount++;
+    }
+    else
+    {
+        Console.WriteLine("Tails");
+        tailCount++;
+    }
+}
+
+// Output results
+Console.WriteLine($"Num Heads: {headCount} Num Tails: {tailCount}");
