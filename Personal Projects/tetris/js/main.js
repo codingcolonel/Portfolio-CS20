42c39966bc10b43b9dc18a1fab6daf9c166b2dea
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-05-04 14:47:55 -0600

    trying to learn deltatime (in progress)

--- Personal Projects/tetris/js/main.js
+++ Personal Projects/tetris/js/main.js
@@ -5,148 +5,75 @@ let ctx = cnv.getContext('2d');
 cnv.width = 660;
 cnv.height = 650;
 
-// Object array
-let blocks = [];
-
 // Global variables
 let score = 10000;
 let level = 20;
 let lines = 47;
+let frames = 0;
+let interval = 60;
+
+var lastTime = null;
+var frameRate = 1000 / 60;
 
 // Deltatime function
-window.requestAnimationFrame(update);
-function update() {
-  // RUN HELPER FUNCTIONS
-  collision();
-  // gameOverScreen();
+// requestAnimationFrame(deltaTime);
+// function deltaTime() {
+//   if (lastTime == null) {
+//     lastTime = new Date().getTime();
+//   }
+//   var dt = new Date().getTime() - lastTime;
+//   lastTime = new Date().getTime();
+//   var updateCount = dt / frameRate;
 
-  // REQUEST ANIMATION FRAME
-  window.requestAnimationFrame(update);
-}
+//   for (var i = 0; i < updateCount; i++) {
+//     update();
+//   }
 
-draw();
-function draw() {
-  // console.log('draw');
-  // DRAW BACKROUND
-  ctx.fillStyle = 'PaleTurquoise';
-  ctx.fillRect(0, 0, 750, 650);
+//   requestAnimationFrame(deltaTime);
+// }
 
-  // Draw game area
-  ctx.fillStyle = 'White';
-  ctx.fillRect(180, 30, 300, 600);
+// let time_step = 1000 / 60,
+//   delta = 0,
+//   last_frame_time_ms = 0, // The last time the loop was run
+//   max_FPS = 60; // The maximum FPS we want to allow
 
-  // Draw boxes
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 2;
-  for (let i = 180; i < 470; i += 30) {
-    for (let j = 30; j <= 600; j += 30) {
-      ctx.strokeRect(i, j, 30, 30);
-    }
+// function gameLoop(timestamp) {
+//   // Throttle the frame rate.
+//   if (timestamp < last_frame_time_ms + 1000 / max_FPS) {
+//     requestAnimationFrame(gameLoop);
+//     return;
+//   }
+
+//   delta += timestamp - last_frame_time_ms;
+//   last_frame_time_ms = timestamp;
+
+//   let num_update_steps = 0;
+//   while (delta >= time_step) {
+//     // update our game logic before draw things to canvas
+//     update(time_step);
+//     delta -= time_step;
+//   }
+
+//   // Call draw function to draw our logo to canvas
+//   update();
+
+//   // Call gameLoop recursevly
+//   requestAnimationFrame(gameLoop);
+// }
+
+function update(time_step) {
+  console.log(time_step);
+  frames++;
+  if (frames % interval === 0) {
+    frames = frames / interval;
+    console.log(frames);
   }
-
-  // Draw Outline
-  ctx.strokeStyle = 'darkcyan';
-  ctx.lineWidth = 10;
-  ctx.strokeRect(174, 24, 312, 612);
-
-  // Draw Hold Box
-  ctx.fillStyle = 'darkcyan';
-  ctx.fillRect(10, 20, 150, 125);
-
-  // Draw Hold Text
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.textAlign = 'center';
-  ctx.fillStyle = 'black';
-
-  ctx.fillText('HOLD', 85, 45);
-
-  // Draw Hold Inner Box
-  ctx.fillStyle = '#eafbfb';
-  ctx.fillRect(20, 50, 130, 85);
-
-  // Draw Next Box
-  ctx.fillStyle = 'darkcyan';
-  ctx.fillRect(500, 20, 150, 375);
-
-  // Draw Next Text
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.fillText('NEXT', 575, 45);
-
-  // Draw Next Inner Box
-  ctx.fillStyle = '#eafbfb';
-  ctx.fillRect(510, 50, 130, 335);
-
-  // Draw Scoreboard Box
-  ctx.fillStyle = 'darkcyan';
-  ctx.fillRect(10, 392, 150, 249);
-
-  // Draw Score Text
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.fillText('SCORE', 85, 443);
-
-  // Draw Score Inner Box
-  ctx.fillStyle = '#eafbfb';
-  ctx.fillRect(20, 448, 130, 25);
-
-  // Draw Score Number
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.textAlign = 'center';
-  ctx.fillText(score, 85, 470);
-
-  // Draw Level Text
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.fillText('LEVEL', 85, 513);
-
-  // Draw Level Inner Box
-  ctx.fillStyle = '#eafbfb';
-  ctx.fillRect(20, 518, 130, 25);
-
-  // Draw Level Number
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.fillText(level, 85, 540);
-
-  // Draw Lines Text
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.fillText('LINES', 85, 583);
-
-  // Draw Lines Inner Box
-  ctx.fillStyle = '#eafbfb';
-  ctx.fillRect(20, 587, 130, 25);
-
-  // Draw Lines Number
-  ctx.font = '25px Verdana, sans-serif';
-  ctx.fillStyle = 'black';
-  ctx.fillText(lines, 85, 610);
-
-  // Draw Tetris Logo
-  let htmlImg = document.getElementById('logo');
-  ctx.drawImage(htmlImg, 495, 535, 160, 105);
-  // 500, 460, 160, 105);
-
-  // Draw Pause Icon
-  ctx.fillStyle = 'darkcyan';
-  ctx.fillRect(548, 435, 15, 60);
-  ctx.fillRect(579, 435, 15, 60);
-
-  // ctx.font = '40px Monospace';
-  // ctx.fillStyle = 'black';
-  // ctx.fillText(score, 45, 640);
-
-  // DRAW ALL SNAKE PARTS
-  // blocks.forEach(drawBlocks);
-
-  // DRAW BANANA
-  // ctx.drawImage(htmlImg2, banana.x, banana.y, 40, 40);
+  // console.log(frames);
 }
 
-function collision() {
-  // console.log('update');
+setInterval(test, 1000);
+function test() {
+  console.log('test');
 }
 
 // TO DO
